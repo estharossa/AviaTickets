7e4675ab2f478147f88aecea4e4a30da54eef0ed
d.amangeldy@aviata.me <OlZwfhz77>
2021-05-15 09:51:52 +0600

    added photo field to user

--- tickets/auth_/models.py
+++ tickets/auth_/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.base_user import BaseUserManager
 from auth_.utils.constants import *
+from .utils.validators import validate_size, validate_extension
 
 
 class MainUserManager(BaseUserManager):
@@ -34,12 +35,9 @@ class MainUserManager(BaseUserManager):
 
 class MainUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(_('first name'), max_length=30, blank=True)
-    last_name = models.CharField(_('last name'), max_length=30, blank=True)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
     is_active = models.BooleanField(_('active'), default=True)
     is_staff = models.BooleanField(_('is_staff'), default=False)
-    role = models.SmallIntegerField(choices=USER_ROLES, default=USER_ROLE_CLIENT)
 
     objects = MainUserManager()
 
@@ -52,13 +50,16 @@ class MainUser(AbstractBaseUser, PermissionsMixin):
 
 
 class Profile(models.Model):
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
     bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+    photo = models.ImageField(upload_to='profile_photos', validators=[validate_size, validate_extension], null=True,
+                              blank=True)
     user = models.OneToOneField(MainUser, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.first_name
+        return self.first_name
 
     class Meta:
         verbose_name = 'Profile'
--- tickets/tickets/settings.py
+++ tickets/tickets/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import datetime
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,6 +138,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
